8daf630411bcdea0f8d4359b4085d91de2808b9e
kayleighmaiergillies <kgillies@mcad.edu>
2026-04-13 10:10:07 -0700

    Add files via upload

--- week11/assignment11/src/App.jsx
+++ week11/assignment11/src/App.jsx
@@ -1,4 +1,13 @@
 import './css/app.css'
+
+function Header() {
+  return (
+    <header>  
+      <h1>Portfolio</h1>
+    </header>
+  );
+}
+
 function ProjectCard(props) { 
   return (
     <div>
@@ -7,12 +16,25 @@ function ProjectCard(props) {
     </div>
   );
 }
+
+function Footer() {
+  return (
+    <footer>
+      <p>Kayleigh Gillies 2026</p>
+    </footer>
+  );
+}
+
 function App() {
   return (
     <div>
       <p>Hello World</p>
-      <ProjectCard name="Project Name" description="Project Description" />
+      <ProjectCard name="Project One" description="Project Description" />
+      <ProjectCard name="Project Two" description="Project Description" />
+      <ProjectCard name="Project Three" description="Project Description" /> 
+      <Footer />  
     </div>
+
   );
 }
 
