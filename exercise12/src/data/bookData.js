a8465dee1aa292ba82ab44c663e794518ec8e14d
kayleighmaiergillies <kgillies@mcad.edu>
2026-04-17 18:13:47 -0700

    Add files via upload

---
+++ exercise12/src/data/bookData.js
@@ -0,0 +1,21 @@
+export const books = [
+{
+  id: 1, 
+  title: "The Signature of All Things",
+  author: "Elizabeth Gilbert",
+  description: "The Signature of All Things is a historical novel by Elizabeth Gilbert, published in 2014, that follows the life of Alma Whittaker, a brilliant 19th-century botanist, as she navigates science, love, and self-discovery across the globe. The story spans the 18th and 19th centuries, focusing on Alma's journey from a wealthy Philadelphia family, her deep study of mosses and evolution, and her complex relationship with an orchid painter, exploring themes of science, spirituality, and the search for meaning. ",
+},
+{
+  id: 2, 
+  title: "Flight Behavior",
+  author: "Barbara Kingsolver",
+  description: "Flight Behavior is a 2012 novel by Barbara Kingsolver about Dellarobia Turnbow, a young, unhappily married woman in rural Tennessee who discovers a massive, inexplicable swarm of monarch butterflies on her family's land, which she initially mistakes for a miracle. Her discovery draws scientists, media, and religious figures, forcing her to confront her own life, her community's denial, and the realities of climate change, as the butterflies' presence is a symptom of environmental disruption. The novel explores themes of science vs. faith, denial, and a woman's search for independence, all through the lens of a global crisis made personal. ",
+},
+{
+  id: 3, 
+  title: "State of Wonder",
+  author: "Ann Patchett",
+  description: "State of Wonder is a 2011 novel by Ann Patchett about research scientist Dr. Marina Singh, who travels to the Amazon rainforest to investigate the mysterious death of a colleague and the work of her former mentor, Dr. Annick Swenson, who is researching a fertility drug. The book is a blend of adventure, mystery, and medical fiction, exploring themes of science, ethics, mortality, and the complexities of human relationships against a lush, dangerous backdrop.",
+
+},
+];
