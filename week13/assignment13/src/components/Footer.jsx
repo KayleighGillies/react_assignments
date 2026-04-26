512def883dcd0b40fa1186695085919f72f92f4c
kayleighmaiergillies <kgillies@mcad.edu>
2026-04-25 20:20:19 -0700

    Delete week13 directory

--- week13/assignment13/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>Kayleigh Gillies 2026</p>
-    </footer>
-  );
-}
-export default Footer;
--- week13/assignment13/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>  
-      <h1>Kayleigh Gillies</h1>
-      <p>Programming Portfolio</p>
-    </header>
-  );
-}
-export default Header;
