78f14157ccea599166874d2f3d6361834e5c932f
kayleighmaiergillies <kgillies@mcad.edu>
2026-04-25 20:13:15 -0700

    Delete week13 directory

--- week13/assignment13/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import ProjectList from "../components/ProjectList.jsx";
-import { projects } from "../data/projectData.js";
-
-function Home() {
-  const limitedProjects = projects.slice(0, 2); // Get the first 3 projects from the array
-  return <ProjectList projects={limitedProjects} />;
-}
-export default Home;
